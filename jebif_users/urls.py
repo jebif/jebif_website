44bf6643a3fbe3dc62449d08f4b8d9bf588fc9cd
Alex Lereveur <12518060-magiclex@users.noreply.gitlab.com>
2025-08-19 18:35:41 +0200

    Changed the deconnexion method, now using a button in the navbar

--- jebif_users/urls.py
+++ jebif_users/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='jebif_users/login.html'), name='login'),
-    path('logout/', views.logout, name="logout"),
+    path('logout/', views.logout, name='logout'),
+    path('button_logout/', views.logout, name='button_logout'),
     path('register/', RegisterView.as_view(), name='register'),
 ]
