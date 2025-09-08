15f6562d85eeaf45669dc6aea3b6e4f79747a5d0
Alex Lereveur <12518060-magiclex@users.noreply.gitlab.com>
2025-09-08 19:13:35 +0200

    Modified the configuration with the base, to include the sidebar in the base  (instead of each html page). Modified the anvbar, with buttons now on top and a better blue. Modified the order of the urls, which allows to remove /website/ from the url

--- jebif_main/settings.py
+++ jebif_main/settings.py
@@ -173,17 +173,18 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 
 # To redirect after connexion (maybe will be changed)
-LOGIN_REDIRECT_URL = '/website/'
+LOGIN_REDIRECT_URL = '/'
 
 
 #### Settings for old
 ROOT_URL="app/"   
-HTTP_DOMAIN="http://jebif.fr"
+HTTP_DOMAIN="http://jebif.fr"   #OK
 LOGIN_URL = f"/{ROOT_URL}accounts/login/"
 #LOGIN_REDIRECT_URL = f"/{ROOT_URL}membership/subscription/me/update/"
 REQUIRE_LOGIN_PATH = LOGIN_URL
-EMAIL_SUBJECT_PREFIX = "[JeBiF] "
-SERVER_EMAIL="admin@jebif.fr"
+EMAIL_SUBJECT_PREFIX = "[JeBiF] " #OK
+SERVER_EMAIL="admin@jebif.fr"   #OK
+
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
