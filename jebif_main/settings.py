7afb99b4c6ba514643e7c9ba843fcec786bea1a4
Benjamin Loire <benjamin.loire@proton.me>
2026-02-16 23:43:16 +0100

    add jebif.fr as domain name

--- jebif_main/settings.py
+++ jebif_main/settings.py
@@ -132,7 +132,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'fr_fr'
+LANGUAGE_CODE = 'fr-fr'
 
 TIME_ZONE = 'UTC'
 
